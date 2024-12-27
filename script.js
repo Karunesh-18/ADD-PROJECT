17dfb437cddabc62fa5a27c560db8861fc5b0d58
Karunesh <karunesh.ar2024cse@sece.ac.in>
2024-12-27 06:00:09 +0530

    Added Scar16

--- script.js
+++ script.js
@@ -10,6 +10,12 @@ function updateCartCount() {
 }
 
 function viewCart() {
-    let cartItems = cart.map(item => `${item.productName} - $${item.price.toFixed(2)}`).join('\n');
-    alert(`Your Cart:\n\n${cartItems}\n\nTotal: $${cart.reduce((total, item) => total + item.price, 0).toFixed(2)}`);
+    let cartItems = cart.map(item => `${item.productName} - ₹${item.price.toFixed(2)}`).join('\n');
+    alert(`Your Cart:\n\n${cartItems}\n\nTotal: ₹${cart.reduce((total, item) => total + item.price, 0).toFixed(2)}`);
+}
+
+function buyNow(productName, price) {
+    cart = [{ productName, price }];
+    updateCartCount();
+    viewCart();
 }
